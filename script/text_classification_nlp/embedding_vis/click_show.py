e6c7788e2779f86740c4693ac74512cfe88a9181
Hu Hao <35623982+Huhaobangbangbang@users.noreply.github.com>
2022-07-07 20:07:22 +0800

    Update click_show.py

--- script/text_classification_nlp/embedding_vis/click_show.py
+++ script/text_classification_nlp/embedding_vis/click_show.py
@@ -1,67 +1,51 @@
 """
  -*- coding: utf-8 -*-
  author： Hao Hu
- @date   2022/6/30 7:16 PM
+ @date   2022/6/30 7:49 PM
 """
-import gensim, logging, os
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-from cope_dataset import get_unrepeat_txt
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-from nltk.tokenize import word_tokenize
-import nltk
-nltk.download('punkt')
 import numpy as np
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from sentence_transformers import SentenceTransformer
+import matplotlib.pyplot as plt
 
 
+def onpick(event):
+    thisline = event.artist
+    xdata = thisline.get_xdata()
+    ydata = thisline.get_ydata()
+    ind = event.ind
+    points = tuple(zip(xdata[ind], ydata[ind]))
+    print('the point of the sentence', x_to_sentence[xdata[ind][0]])
 
-def cosine(u, v):
-    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+def get_pos():
+    """得到点位置信息"""
+    with open('inf.txt','r') as fp:
+        contents = fp.readlines()
+    pos = []
+    x_to_sentence = {}
+    for sample in contents:
+        x = float(sample.split()[0])
+        y = float(sample.split()[1])
+        pos.append([x,y])
+        sentence_str = ''
+        for sample in  sample.split()[2:]:
+            sentence_str +=' '+sample
+        x_to_sentence[x] = sentence_str
+
+    return pos,x_to_sentence
 
 
-def click_show():
-    """定义一个点击事件，一点击就显示对应的句子"""
-
-def tsne_plot(tokens, labels):
-    """利用tsne生成图片"""
-    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
-    new_values = tsne_model.fit_transform(tokens)
-    x = []
-    y = []
-    for value in new_values:
-        x.append(value[0])
-        y.append(value[1])
-    plt.figure(figsize=(32, 32))
-    for i in range(len(x)):
-        plt.scatter(x[i], y[i])
-        plt.annotate(labels[i],
-                     xy=(x[i], y[i]),
-                     xytext=(5, 2),
-                     textcoords='offset points',
-                     ha='right',
-                     va='bottom')
+def show_pic(pos,x_to_sentence):
+    fig, ax = plt.subplots()
+    ax.set_title('click and show sentence')
+    for i in range(len(pos)):
+        line, = ax.plot(pos[i][0], pos[i][1], 'o',
+                        picker=True, pickradius=5)  # 5 points tolerance
+    fig.canvas.mpl_connect('pick_event', onpick)
     plt.show()
 
 
 
-def train(str_en_text):
-    tokenized_sent = []
-
-    for s in str_en_text:
-        tokenized_sent.append(word_tokenize(s.lower()))
-    tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(tokenized_sent)]
-    sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
-    sentence_embeddings = sbert_model.encode(str_en_text)  # 得到了每一个句子的向量
-    tsne_plot(sentence_embeddings[:88], str_en_text[:88])
-
 if __name__ == '__main__':
-    json_path = '/cloud/cloud_disk/users/huh/dataset/nlp_dataset/question_dataset/ori/en_ch_cattree_personality.json'
-    str_en_text = get_unrepeat_txt()
-    train(str_en_text[:100])
-
-
-
-
-
+    # fig.canvas.mpl_connect('pick_event', onpick)
+    # plt.show()
+    pos,x_to_sentence = get_pos()
+    show_pic(pos, x_to_sentence)
